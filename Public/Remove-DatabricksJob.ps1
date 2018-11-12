ecb0bd6b1f8a45638825ac1f6779c540f99ef51b
tadziqusky <12574467+tadziqusky@users.noreply.github.com>
2018-11-12 09:45:52 +0100

    Added Remove Job script, fixed issue with not returing jobId if job already exsists (#13)
    * Added script for removing DatabricksJob based on JobId
    
    * Changed Write-Output to Write-Verbose (Write-Output causes returning array with jobId and WriteOutput value after running script instead of only jobId;
    Fixed typo $Moode to $Mode
    Added param to set if remove job after last test or not

---
+++ Public/Remove-DatabricksJob.ps1
@@ -0,0 +1,48 @@
+Function Remove-DatabricksJob
+{ 
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory = $true)][string]$BearerToken, 
+        [parameter(Mandatory = $true)][string]$Region,
+        [parameter(Mandatory = $false)][string]$JobId
+    ) 
+<#
+.SYNOPSIS
+Delete a job from Databricks with given Job Id
+
+.DESCRIPTION
+Delete a job from Databricks with given Job Id 
+
+.PARAMETER BearerToken
+Your Databricks Bearer token to authenticate to your workspace (see User Settings in Datatbricks WebUI)
+
+.PARAMETER Region
+Azure Region - must match the URL of your Databricks workspace, example northeurope
+
+.PARAMETER JobId
+Id of the job to delete
+
+.NOTES
+Author: Tadeusz Balcer
+#>  
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $InternalBearerToken =  Format-BearerToken($BearerToken) 
+    $Region = $Region.Replace(" ","")
+    
+    $Body = @{}
+    $Body['job_id'] = $JobId
+
+    $BodyText = $Body | ConvertTo-Json -Depth 10
+    
+    Try {
+        Invoke-RestMethod -Method Post -Body $BodyText -Uri "https://$Region.azuredatabricks.net/api/2.0/jobs/delete" -Headers @{Authorization = $InternalBearerToken}
+    }
+    Catch {
+        Write-Output "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+        Write-Output "StatusDescription:" $_.Exception.Response.StatusDescription
+        Write-Error $_.ErrorDetails.Message
+    }
+
+    Return 
+}
+    
