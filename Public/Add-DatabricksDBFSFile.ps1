1b7b08591d453af4c54b3e974b37ef0a714370bd
Simon D'Morias <simon@Simons-MacBook-Pro.local>
2018-11-12 12:33:33 +0000

    fix file copy on windows

--- Public/Add-DatabricksDBFSFile.ps1
+++ Public/Add-DatabricksDBFSFile.ps1
@@ -41,6 +41,8 @@ Function Add-DatabricksDBFSFile {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $InternalBearerToken = Format-BearerToken($BearerToken)
     $Region = $Region.Replace(" ","")
+    $LocalRootFolder = Resolve-Path $LocalRootFolder
+    Write-Verbose $LocalRootFolder
     $uri ="https://$Region.azuredatabricks.net/api/2.0/dbfs/put"
     Push-Location
     Set-Location $LocalRootFolder
@@ -48,24 +50,25 @@ Function Add-DatabricksDBFSFile {
     $AllFiles = Get-ChildItem -Filter $FilePattern -Recurse -File
 
     Foreach ($f in $AllFiles){
+        $f = Resolve-Path $f.FullName
         Write-Verbose "Replacing CRLF with LF for $f"
         $text = [IO.File]::ReadAllText($f) -replace "`r`n", "`n"
         [IO.File]::WriteAllText($f, $text)
 
-        Write-Verbose "Encoding $f to BASE64"
         $BinaryContents = [System.IO.File]::ReadAllBytes($f)
         $EncodedContents = [System.Convert]::ToBase64String($BinaryContents)
-
+        Write-Verbose "TargetLocation: $TargetLocation"
         $FileTarget = (Join-Path $TargetLocation (Resolve-Path $f -Relative))
+        Write-Verbose "FileTarget: $FileTarget"
         $FileTarget = $FileTarget.Replace("\","/")
         $FileTarget = $FileTarget.Replace("/./","/")
-        
+
         $Body = @{"contents"=$EncodedContents}
         $Body['path'] = $FileTarget
         $Body['overwrite'] = "true"
     
         $BodyText = $Body | ConvertTo-Json -Depth 10
-        Write-Output "Pushing file $($f.FullName) to $FileTarget to REST API: $uri"
+        Write-Verbose "Pushing file $($f.FullName) to $FileTarget to REST API: $uri"
         Invoke-RestMethod -Uri $uri -Body $BodyText -Method 'POST' -Headers @{Authorization = $InternalBearerToken}
     }
     Pop-Location
