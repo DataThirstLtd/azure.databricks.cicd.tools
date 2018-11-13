613ff474cca8ca63a113add902b849222128627c
Simon D'Morias <simon@Simons-MacBook-Pro.local>
2018-11-13 14:19:29 +0000

    skippub

--- Tests/_InvokeTests.ps1
+++ Tests/_InvokeTests.ps1
@@ -3,7 +3,7 @@
 $PSVersionTable
 
 Get-Module
-Install-Module Pester -MinimumVersion 4.4.2 -Force -Scope CurrentUser
+Install-Module Pester -MinimumVersion 4.4.2 -Force -Scope CurrentUser -SkipPublisherCheck
 Import-Module Pester -MinimumVersion 4.4.2
 Get-Module
 Set-Location $PSScriptRoot
