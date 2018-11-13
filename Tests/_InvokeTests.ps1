916c3a4c168ea633c38a45fbba316ff810eed40e
Simon D'Morias <simon@Simons-MacBook-Pro.local>
2018-11-13 14:07:09 +0000

    force pester version

--- Tests/_InvokeTests.ps1
+++ Tests/_InvokeTests.ps1
@@ -3,7 +3,7 @@
 $PSVersionTable
 
 Get-Module
-Import-Module Pester
+Import-Module Pester -MinimumVersion 4.4.2
 Get-Module
 Set-Location $PSScriptRoot
 Invoke-Pester -Script ./*.tests.ps1 -OutputFile TestResults.xml -OutputFormat NUnitXML
