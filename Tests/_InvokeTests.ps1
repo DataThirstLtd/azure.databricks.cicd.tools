095941acadbd263aef80186f2d418487164ab536
Simon D'Morias <simon@Simons-MacBook-Pro.local>
2018-11-13 14:42:53 +0000

    tidy up final file

--- Tests/_InvokeTests.ps1
+++ Tests/_InvokeTests.ps1
@@ -1,11 +1,6 @@
-
-
-$PSVersionTable
-
-Get-Module
 Install-Module Pester -MinimumVersion 4.4.2 -Force -Scope CurrentUser -SkipPublisherCheck -Confirm
 Import-Module Pester -MinimumVersion 4.4.2
-Get-Module
+
 Set-Location $PSScriptRoot
 Invoke-Pester -Script ./*.tests.ps1 -OutputFile TestResults.xml -OutputFormat NUnitXML
 Set-Location $PSScriptRoot
