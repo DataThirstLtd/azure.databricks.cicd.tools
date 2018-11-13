6dc145f55c870aaebe6509b846f5b9155f9f17ef
Simon D'Morias <simon@Simons-MacBook-Pro.local>
2018-11-13 14:05:00 +0000

    fix ci

--- Tests/_InvokeTests.ps1
+++ Tests/_InvokeTests.ps1
@@ -1,4 +1,10 @@
 
+
+$PSVersionTable
+
+Get-Module
+Import-Module Pester
+Get-Module
 Set-Location $PSScriptRoot
 Invoke-Pester -Script ./*.tests.ps1 -OutputFile TestResults.xml -OutputFormat NUnitXML
 Set-Location $PSScriptRoot
