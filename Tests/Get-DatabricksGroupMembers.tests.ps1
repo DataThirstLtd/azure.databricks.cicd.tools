92d15e4595553876e16bcf244b7b1eaa90d41ef0
Tadeusz Balcer <12574467+tadziqusky@users.noreply.github.com>
2019-02-11 19:16:05 +0100

    fix test params

--- Tests/Get-DatabricksGroupMembers.tests.ps1
+++ Tests/Get-DatabricksGroupMembers.tests.ps1
@@ -2,10 +2,11 @@ Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
 $BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt" # Create this file in the Tests folder with just your bearer token in
 
 $Region = "west europe" 
+$GroupName = "testgroup"
 
 Describe "Get-DatabricksGroupMembers" {
     It "Simple fetch" {
-        $members = Get-DatabricksGroupMembers -BearerToken $BearerToken -Region $Region -testgroup
+        $members = Get-DatabricksGroupMembers -BearerToken $BearerToken -Region $Region -GroupName $GroupName 
     }
 }
 
