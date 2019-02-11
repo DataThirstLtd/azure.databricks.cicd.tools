c002f0586190c75e4d7a408a02c4cc8d591a95fa
Tadeusz Balcer <12574467+tadziqusky@users.noreply.github.com>
2019-02-11 19:24:13 +0100

    Add-DatabricksGroup function

---
+++ Public/Add-DatabricksGroup.ps1
@@ -0,0 +1,69 @@
+<#
+
+.SYNOPSIS
+    Create a user group in a Databricks instance.
+
+.DESCRIPTION
+    Create a user group in a Databricks instance.
+
+.PARAMETER BearerToken
+    Your Databricks Bearer token to authenticate to your workspace (see User Settings in Databricks WebUI)
+
+.PARAMETER Region
+    Azure Region - must match the URL of your Databricks workspace, example northeurope
+
+.PARAMETER GroupName
+    Name for the new group.
+
+.EXAMPLE 
+C:\PS> Add-DatabricksGroup -BearerToken $BearerToken -Region $Region -GroupName acme
+
+This example creates a group called acme
+
+.NOTES
+    Author: Tadeusz Balcer
+
+#>
+
+
+Function Add-DatabricksGroup
+{
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory=$true)][string]$BearerToken,
+        [parameter(Mandatory=$true)][string]$Region,
+        [parameter(Mandatory=$true)][string]$GroupName
+    )
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    
+    $InternalBearerToken = Format-BearerToken($BearerToken)
+    $Region = $Region.Replace(" ","")
+    
+
+    Try
+    {
+        Get-Databricks
+
+        $body = '{"group_name": "' + $GroupName + '"  }'
+
+        Invoke-RestMethod -Method Post -Body $body -Uri "https://$Region.azuredatabricks.net/api/2.0/groups/create" -Headers @{Authorization = $InternalBearerToken} -OutFile $OutFile
+        Write-Output "Group $GroupName created"
+    }
+    Catch
+    {
+        $err = $_.ErrorDetails.Message
+        if ($err.Contains('already exists'))
+        {
+            Write-Verbose $err
+        }
+        else
+        {
+            throw
+        }
+    }
+
+}
+
+# Command was renamed to align prefixes
+New-Alias -Name Add-Group -Value Add-DatabricksGroup
---
+++ Tests/Add-DatabricksGroup.tests.ps1
@@ -0,0 +1,11 @@
+Import-Module "$PSScriptRoot\..\azure.databricks.cicd.tools.psm1" -Force
+
+$BearerToken = Get-Content "$PSScriptRoot\MyBearerToken.txt"  # Create this file in the Tests folder with just your bearer token in
+$Region = "westeurope"
+$GroupName = "acme"
+
+Describe "Add-DatabricksGroup" {
+    It "Simple addition"{
+        Add-DatabricksUserGroup -BearerToken $BearerToken -Region $Region -GroupName $GroupName -Verbose
+    }
+}
