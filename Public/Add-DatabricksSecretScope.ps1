a4733cf62d4e206440f3d4b4046d181d8084bbe9
Simon D'Morias <simon@datathirst.net>
2019-02-21 14:30:23 +0000

    key vault scopes

--- Public/Add-DatabricksSecretScope.ps1
+++ Public/Add-DatabricksSecretScope.ps1
@@ -5,7 +5,7 @@
 
 .DESCRIPTION
     Create a scope to store Databricks secret in. Note the the Set-DatabricksSecret command creates the scope if it does not exist.
-    Note that from version 1.9 all scopes are created with a generic permission to allow all users to access it.
+    Populate KeyVaultResourceId to create a scope from a Key Vault
 
 .PARAMETER BearerToken
     Your Databricks Bearer token to authenticate to your workspace (see User Settings in Databricks WebUI)
@@ -15,6 +15,15 @@
 
 .PARAMETER ScopeName
     Name for the scope - do not include spaces or special characters.
+
+.PARAMETER KeyVaultResourceId
+    Resource ID for a Key Vault to attach this scope to an Azure Key Vault. Should be in the URI form, 
+    see the properties blade of your Key Vault and copy the RESOURCE ID
+    THIS IS IN PREVIEW AND NOT OFFICALLY SUPPORTED BY DATABRICKS YET
+
+.PARAMETER AllUserAccess
+    By default only the user creating the scope has access to secrets. When you set this flag all users will
+    have access. Hopefully better permissions controls will come.
 
 .EXAMPLE 
 C:\PS> Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName "Test1"
@@ -33,19 +42,37 @@ Function Add-DatabricksSecretScope
     param (
         [parameter(Mandatory=$true)][string]$BearerToken,
         [parameter(Mandatory=$true)][string]$Region,
-        [parameter(Mandatory=$true)][string]$ScopeName
+        [parameter(Mandatory=$true)][string]$ScopeName,
+        [parameter(Mandatory=$false)][string]$KeyVaultResourceId,
+        [parameter(Mandatory=$false)][switch]$AllUserAccess
     )
 
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     
     $InternalBearerToken = Format-BearerToken($BearerToken)
     $Region = $Region.Replace(" ","")
-    $body = '{"scope": "' + $ScopeName + '", "initial_manage_principal": "users"}'
+    $body = @{}
+    $body['scope'] = $ScopeName
+
+    if ($PSBoundParameters.ContainsKey('AllUserAccess')){$body['initial_manage_principal'] = "users"}
+
+    # Key Vault backed Scope (This is in preview only)
+    if ($PSBoundParameters.ContainsKey('KeyVaultResourceId')){
+        $kv = @{}
+        $kv['resource_id'] = $KeyVaultResourceId
+        $LastPart = $KeyVaultResourceId.split('/')[-1]
+        $kv['dns_name'] = "https://$LastPart.vault.azure.net/"
+        
+        $body['scope_backend_type'] = 2
+        $body['backend_azure_keyvault'] = $kv
+    }
+    
+    $BodyText = $Body | ConvertTo-Json -Depth 10
 
     Try
     {
-        Invoke-RestMethod -Method Post -Body $body -Uri "https://$Region.azuredatabricks.net/api/2.0/secrets/scopes/create" -Headers @{Authorization = $InternalBearerToken} -OutFile $OutFile
-        Write-Output "Secret Scope $ScopeName created"
+        Invoke-RestMethod -Method Post -Body $BodyText -Uri "https://$Region.azuredatabricks.net/ajax-api/2.0/secrets/scopes/create" -Headers @{Authorization = $InternalBearerToken} -OutFile $OutFile
+        Write-Verbose "Secret Scope $ScopeName created"
     }
     Catch
     {
--- Tests/Add-DatabricksSecretScope.tests.ps1
+++ Tests/Add-DatabricksSecretScope.tests.ps1
@@ -2,11 +2,22 @@ Set-Location $PSScriptRoot
 Import-Module "..\azure.databricks.cicd.Tools.psd1" -Force
 $BearerToken = Get-Content "MyBearerToken.txt"  # Create this file in the Tests folder with just your bearer token in
 $Region = "westeurope"
-
-$ScopeName = "DataThirst"
+$ResID = "/subscriptions/b146ae31-d42f-4c88-889b-318f2cc23f98/resourceGroups/dts-dev-ne-rg/providers/Microsoft.KeyVault/vaults/dts-kv-dev-nedt"
 
 Describe "Add-DatabricksSecretScope" {
+    BeforeAll{
+            Remove-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName "Normal"
+            Remove-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName "KVScope"
+    }
     It "Simple addition"{
-        $Res = Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName $ScopeName  -Verbose
+        Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName "Normal"  -Verbose
+    }
+
+    It "All User Access"{
+        Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName "NormalWithPermissions" -AllUserAccess  -Verbose
+    }
+
+    It "Key Vault addition"{
+        Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName "KVScope" -KeyVaultResourceId $ResID  -Verbose
     }
 }
