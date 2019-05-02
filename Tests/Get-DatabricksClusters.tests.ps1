52f9f6a000ffa20e08db502f4d2546d5b1a8d0e8
Simon D'Morias <simon@datathirst.net>
2019-05-02 14:06:12 +0100

    default new clusters to Python 3 (#53)

--- Tests/Get-DatabricksClusters.tests.ps1
+++ Tests/Get-DatabricksClusters.tests.ps1
@@ -3,18 +3,18 @@ Import-Module "..\azure.databricks.cicd.Tools.psd1" -Force
 $BearerToken = Get-Content "MyBearerToken.txt"  # Create this file in the Tests folder with just your bearer token in
 $Region = "westeurope"
 
+
+
 Describe "Get-DatabricksClusters"{
     
     It "Get all clusters"{
         $json = Get-DatabricksClusters -BearerToken $BearerToken -Region $Region
-        $Json.clusters.Count | Should -BeGreaterThan 1
+        $Json.Count | Should -BeGreaterThan 1
     }
 
     It "Search for cluster by id"{
         $json = Get-DatabricksClusters -BearerToken $BearerToken -Region $Region
         $ClusterId = $Json.cluster_id[0]
-        $ClusterName = $Json.cluster_name[0]
-        Write-host $ClusterId 
         $json = Get-DatabricksClusters -BearerToken $BearerToken -Region $Region -ClusterId $ClusterId
         $Json.cluster_id | Should -Be "$ClusterId"
     }
