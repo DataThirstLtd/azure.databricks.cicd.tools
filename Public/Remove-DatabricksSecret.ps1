305b65d6a4e44ca52d99447c4170792ef4333365
Simon D'Morias <simon@datathirst.net>
2019-10-24 11:58:22 +0100

    update notes

--- Public/Remove-DatabricksSecret.ps1
+++ Public/Remove-DatabricksSecret.ps1
@@ -1,11 +1,10 @@
 <#
 
 .SYNOPSIS
-    Create a scope to store Databricks secret in.
+    Delete a Databricks secret.
 
 .DESCRIPTION
-    Create a scope to store Databricks secret in. Note the the Set-DatabricksSecret command creates the scope if it does not exist.
-    Populate KeyVaultResourceId to create a scope from a Key Vault
+    Delete a Databricks secret.
 
 .PARAMETER BearerToken
     Your Databricks Bearer token to authenticate to your workspace (see User Settings in Databricks WebUI)
@@ -22,7 +21,7 @@
 .EXAMPLE 
 C:\PS> Remove-DatabricksSecret -BearerToken $BearerToken -Region $Region -ScopeName "Test1" -SecretName "Test"
 
-This example creates a scope called Test1
+This example removes a secret called Test in Scope Test1
 
 .NOTES
     Author: Simon D'Morias / Data Thirst Ltd
