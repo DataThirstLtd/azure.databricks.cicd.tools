0fd0f1ac62796083c44011d4c0327bd04399a01c
Richie Lee <richie@bzzzt.io>
2019-12-09 16:14:44 +0000

    add alluseraccess as a switch to set-dbsecret so that adddbscope can be called with option (#96)

--- Public/Set-DatabricksSecret.ps1
+++ Public/Set-DatabricksSecret.ps1
@@ -15,6 +15,10 @@
 .PARAMETER ScopeName
     Name for the scope - do not include spaces or special characters.
 
+.PARAMETER AllUserAccess
+    By default only the user creating the scope has access to secrets. When you set this flag all users will
+    have access. Hopefully better permissions controls will come.
+
 .EXAMPLE 
     C:\PS> Set-DatabricksSecret -BearerToken $BearerToken -Region $Region -ScopeName "Test1" -SecretName 'MySecretName' -SecretValue 'P@ssword'
 
@@ -25,22 +29,26 @@
 
 #>
 
-Function Set-DatabricksSecret
-{
+Function Set-DatabricksSecret {
     [cmdletbinding()]
     param (
-        [parameter(Mandatory=$false)][string]$BearerToken,
-        [parameter(Mandatory=$false)][string]$Region,
-        [parameter(Mandatory=$true)][string]$ScopeName,
-        [Parameter(Mandatory=$true)][string]$SecretName,
-        [Parameter(Mandatory=$true)][string]$SecretValue
+        [parameter(Mandatory = $false)][string]$BearerToken,
+        [parameter(Mandatory = $false)][string]$Region,
+        [parameter(Mandatory = $true)][string]$ScopeName,
+        [Parameter(Mandatory = $true)][string]$SecretName,
+        [Parameter(Mandatory = $true)][string]$SecretValue,
+        [parameter(Mandatory = $false)][switch]$AllUserAccess
     )
 
     $Headers = GetHeaders $PSBoundParameters
     
 
-    Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName $ScopeName
-
+    if ($PSBoundParameters.ContainsKey('AllUserAccess') -eq $true) {
+        Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName $ScopeName -AllUserAccess
+    }
+    else {
+        Add-DatabricksSecretScope -BearerToken $BearerToken -Region $Region -ScopeName $ScopeName
+    }
     $body = '{ "scope": "' + $ScopeName + '", "key": "' + $SecretName + '", "string_value": "' + $SecretValue + '"}'
 
     Invoke-RestMethod -Method Post -Body $body -Uri "$global:DatabricksURI/api/2.0/secrets/put" -Headers $Headers
