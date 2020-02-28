7d6ae02730491ecf259a41bf27f260ab9b8a0669
Richie Lee <richie@bzzzt.io>
2020-02-28 08:33:40 +0000

    bumped minor version to 5.1; install threadjob if less than ps6.1;thread importdatabricksfolder (#106)

--- azure.databricks.cicd.tools.psm1
+++ azure.databricks.cicd.tools.psm1
@@ -1,16 +1,13 @@
 #Get public and private function definition files.
-$Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach($import in @($Public + $Private))
-{
-    Try
-    {
+Foreach ($import in @($Public + $Private)) {
+    Try {
         . $import.fullname
     }
-    Catch
-    {
+    Catch {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
     }
 }
@@ -18,3 +15,14 @@ Foreach($import in @($Public + $Private))
 # Export Public functions
 Export-ModuleMember -Function $Public.Basename
 Export-ModuleMember -Alias * -Function *
+
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    $checkThreadJob = Get-InstalledModule -Name "ThreadJob" -MinimumVersion "2.0.3" -ErrorAction SilentlyContinue
+    if ($null -eq $checkThreadJob) {
+        Install-Module -Name ThreadJob -RequiredVersion 2.0.3 -Force -Scope CurrentUser
+    }
+    $checkThreadJob = Get-InstalledModule -Name "ThreadJob" -MinimumVersion "2.0.3" -ErrorAction SilentlyContinue
+    if($null -eq $checkThreadJob){
+        Write-Error "oh dear thread job not installed"
+    }
+}
