2d3539e492dfb3af05cb2f12249279bf5ee69dd3
kingmemon0107 <98257181+kingmemon0107@users.noreply.github.com>
2022-02-10 13:35:58 -0800

    Delete Add-DatabricksChunk.ps1

--- Private/Add-DatabricksChunk.ps1
+++
@@ -1,9 +0,0 @@
-Function Add-DatabricksChunk([string]$part, $handle){
-    $Body = @{"data"=$part}
-    $Body['handle'] = $handle
-    $BodyText = $Body | ConvertTo-Json -Depth 10
-
-    $Headers = GetHeaders $null
-    Invoke-RestMethod -Uri "$global:DatabricksURI/api/2.0/dbfs/add-block" -Body $BodyText -Method 'POST' -Headers $Headers
-    Return
-}
